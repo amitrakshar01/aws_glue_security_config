ac4aa6b549b36c4658cfeee0601cd94db428c74e
AksharSans <sanyalamitrakshar@gmail.com>
2022-10-19 18:22:11 +0530

    .

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+resource "aws_glue_security_configuration" "example" {
+  name = "example"
+
+  encryption_configuration {
+    cloudwatch_encryption {
+      cloudwatch_encryption_mode = "DISABLED"
+    }
+
+    job_bookmarks_encryption {
+      job_bookmarks_encryption_mode = "DISABLED"
+    }
+
+    s3_encryption {
+      kms_key_arn        = ""
+      s3_encryption_mode = "SSE-KMS"
+    }
+  }
+}
